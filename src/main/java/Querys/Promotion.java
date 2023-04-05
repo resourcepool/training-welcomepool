407458d9c301175712debf3925ed3ce734c875c8
Adrien-Sejourne <asejourne@takima.fr>
2023-04-05 18:05:40 +0200

    test DAO promotion

---
+++ src/main/java/Querys/Promotion.java
@@ -0,0 +1,25 @@
+package main.java.Querys;
+
+public class Promotion {
+
+    private int id;
+
+    private String name;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
